5dc5e10b02f3d60ce00b842530b79223a44166ad
al-pass <83602426+al-pass@users.noreply.github.com>
2021-06-19 14:10:59 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let names = document.querySelector('.edit-form__name');
 let desc = document.querySelector('.edit-form__desc');
 let submitBut = document.querySelector ('.edit-form__submit');
-let exisName = document.querySelector('.profile__profile-info_name');
-let exisDesc = document.querySelector('.profile__profile-info_desc');
+let exisName = document.querySelector('.profile__profile-info-name');
+let exisDesc = document.querySelector('.profile__profile-info-desc');
 
 function changeInfo(){
     exisName.textContent = names.value;
@@ -17,7 +17,7 @@ submitBut.onclick = changeInfo;
 
 
 let closeIcon = document.querySelector('.edit-form__close-icon');
-let editIcon = document.querySelector('.profile__profile-info_edit-button');
+let editIcon = document.querySelector('.profile__profile-info-edit-button');
 let popup = document.querySelector ('.edit-form');
 function togglePopup() {
     if (popup.style.display === 'flex'){
